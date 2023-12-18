4ee34ae9372165579976c3a1a9c0b34e6110d6fb
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-12-17 20:47:46 -0500

    header mobile complete

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,23 @@
 import '../Header/Header.scss'
-import Navbar from "../Navbar/Navbar";
-import logo from '../../assets/logo/logocircle.png'
+import Navbar from '../Navbar/Navbar';
+import codingIcon from '../../assets/icons/coding-icon.svg';
+import longArrowIcon from '../../assets/icons/long-arrow.svg'
 
 function Header() {
 
     return (
-        <header className="header">
-            <img className='header__logo' src={logo}></img>
-            <Navbar /> 
-            <article className='header__article'>
-                <h1 className='header__article__hello'>Hi there 👋 , i'm</h1>
-                <h2 className='header__article__name'>christian.</h2>
-                <h3 className='header__article__role'>developer + designer 👨‍💻</h3>
-                <p className='header__article__about'>I’m a creative oriented full-stack developer based in Miami. I love design and bodybuilding!</p>
-                <button className='header__article__button'>Hire me</button>
-            </article>
+        <header className='header'>
+            <Navbar />
+            <section className='hero'>
+                <article className='hero__article'>
+                    <h1 className='hero__article__hi'>Hi! 👋 I'm Christian,</h1>
+                    <h2 className='hero__article__position'>Software <img className='hero__article__coding-icon' src={codingIcon}></img> Engineer And <span className='hero__article__ux-design'>UX Designer</span></h2>
+                    <p className='hero__article__about'>Lover of life’s healthier aspects, I’m into fitness, learning about wellness and psychology, and I enjoy spending my time outside.</p>
+                </article>
+            </section>
+            <div className='header__horizontal-line'>
+                <img className='header__arrow-icon' src={longArrowIcon}></img>
+            </div>
         </header>
     )
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,11 @@
 import "./HomePage.scss";
-import Navbar from "../../Components/Navbar/Navbar";
+import Header from "../../Components/Header/Header";
 
 function HomePage() {
 
     return (
         <>
-            <Navbar />
+            <Header />
         </>
     )
 }
