29db078590ba036efe77efe2e2055df2596cb07b
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-12-28 20:04:28 -0500

    mobile view for footer complete

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,10 +4,10 @@ import logoWhite from '../../assets/logo/logo-white.svg'
 function Footer() {
 
     return (
-        <footer>
-            <img src={logoWhite}></img>
-            <p>Lover of life’s healthier aspects, I’m into fitness, learning about wellness and psychology, and I enjoy spending my time outside. When I can I rejoice in diving into a good book or listening to thought provoking podcasts.</p>
-            <h1>© 2023 Christian Garcia . All rights reserved.</h1>
+        <footer className='footer'>
+            <img className='footer__logo' src={logoWhite}></img>
+            <p className='footer__closer'>Lover of life’s healthier aspects, I’m into fitness, learning about wellness and psychology, and I enjoy spending my time outside. When I can I rejoice in diving into a good book or listening to thought provoking podcasts.</p>
+            <h1 className='footer__copyright'>© 2023 Christian Garcia . All rights reserved.</h1>
         </footer>
     )
 }
