128deb473475bc3e4551155ac726cb8380238a63
christiangarcia-dev <2001christiangarcia@gmail.com>
2024-01-08 14:35:27 -0500

    made the project name dynamic within the swiper

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -10,9 +10,6 @@ import Swiperzzz from '../Swiper/Swiper';
 
 function Projects() {
 
-    const [currentIndex, setCurrentIndex] = useState(0);
-    // const swiperRef = useRef(null);
-
     const projects = [
         { name: 'EchoLingo', image: EchoLingoImage },
         { name: 'DataConnect', image: DataConnectImage },
@@ -20,21 +17,6 @@ function Projects() {
         { name: 'BrainFlix', image: BrainFlixImage },
         { name: 'SpaceTourism', image: SpaceTourismImage },
     ];
-
-    // const itemWidth = () => {
-    //     // Get width of a single item (including margin)
-    //     return swiperRef.current
-    //         ? swiperRef.current.firstChild.offsetWidth + 32 // Assume 16px margin on each side
-    //         : 0;
-    // };
-
-    // const goToNext = () => {
-    //     setCurrentIndex((prevIndex) => (prevIndex + 1) % projects.length);
-    // };
-    
-    // const goToPrev = () => {
-    //     setCurrentIndex((prevIndex) => (prevIndex === 0 ? projects.length - 1 : prevIndex - 1));
-    // };
 
     return (
         <section className='projects'>
@@ -46,27 +28,6 @@ function Projects() {
             <section className='projects__container'>
                 <Swiperzzz />
             </section>
-
-            {/* <section className='projects__swiper' ref={swiperRef}>
-                <div className='projects__swiper__container' style={{ transform: `translateX(-${currentIndex * itemWidth()}px)` }}>
-                    {projects.map((project, index) => (
-                        <img key={index} src={project.image} alt={project.name} className={`projects__swiper__item ${currentIndex === index ? 'active' : ''}`} />
-                    ))}
-                </div>
-                <div className='projects__swiper__name-container'>
-                    <h1 className='projects__swiper__project-name'>{projects[currentIndex].name}</h1>
-                </div>
-            </section>
-
-            <div className='projects__arrow__group'>
-                    <img className='projects__arrow-icon--tablet' src={circleArrowIcon} onClick={goToPrev}></img>
-                    <img className='projects__arrow-icon--tablet' src={circleArrowIcon} onClick={goToNext}></img>
-                </div>
-
-            <div className='projects__horizontal-line'>
-                <img className='projects__arrow-icon' src={circleArrowIcon} onClick={goToNext}></img>
-                <img className='projects__arrow-icon' src={circleArrowIcon} onClick={goToPrev}></img>
-            </div> */}
         </section>
     )
 }
