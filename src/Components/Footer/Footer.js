67ec568b0f273316a6e4b9d5155b2c0026704193
christiangarcia-dev <2001christiangarcia@gmail.com>
2024-01-08 17:31:10 -0500

    projects section highly functional now, still missing links though

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
             </div>
             <img className='footer__logo' src={logoWhite}></img>
             <p className='footer__closer'>Lover of life’s healthier aspects, I’m into fitness, learning about wellness and psychology, and I enjoy spending my time outside. When I can I rejoice in diving into a good book or listening to thought provoking podcasts.</p>
-            <h1 className='footer__copyright'>© 2023 Christian Garcia . All rights reserved.</h1>
+            <h1 className='footer__copyright'>© 2024 Christian Garcia . All rights reserved.</h1>
         </footer>
     )
 }
